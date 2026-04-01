adf20544f47ad528f721e00ea8c41056476e44df
Krishi Khanna <kkhanna704@gmail.com>
2026-04-01 13:50:30 +0530

    Update Mini_project.py

--- Mini_project.py
+++ Mini_project.py
@@ -1,12 +1,12 @@
-import random
-random_number = random.randint(1, 100)
+# import random
+# random_number = random.randint(1, 100)
 
-while True:
-  user_input = int(input("Enter the number: "))
-  if user_input < random_number:
-    print("Too low! Try again.")
-  elif user_input > random_number:
-    print("Too high! Try again.")
-  else:
-    print("Congratulations! You guessed the number.")
-    break
+# while True:
+#   user_input = int(input("Enter the number: "))
+#   if user_input < random_number:
+#     print("Too low! Try again.")
+#   elif user_input > random_number:
+#     print("Too high! Try again.")
+#   else:
+#     print("Congratulations! You guessed the number.")
+#     break
